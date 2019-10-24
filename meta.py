616499c5a14f8dcc52334849a450f231fde5f0c7
Weverson Nascimento <weverson@ufpa.br>
2019-10-24 17:27:05 -0300

    acrescentada tela de instruções para resposta

--- meta.py
+++ meta.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy3 Experiment Builder (v3.2.3),
-    on qui 24 out 2019 16:00:23 -03
+    on qui 24 out 2019 17:25:36 -03
 If you publish work using this script the most relevant publication is:
 
     Peirce J, Gray JR, Simpson S, MacAskill M, Höchenberger R, Sogo H, Kastman E, Lindeløv JK. (2019) 
@@ -88,6 +88,17 @@ texto_tela_inicial = visual.TextStim(win=win, name='texto_tela_inicial',
     languageStyle='LTR',
     depth=0.0);
 key_resp_ti = keyboard.Keyboard()
+
+# Initialize components for Routine "instrucao_resposta"
+instrucao_respostaClock = core.Clock()
+text_ir = visual.TextStim(win=win, name='text_ir',
+    text='Pressione "1" ou "2" caso você reconheça a face humana na primeira ou segunda imagem, respectivamente.\n\nVocê terá dois segundos para responder.\n\nPressione a tecla “Espaço” do seu teclado para prosseguir.',
+    font='Arial',
+    pos=(0, 0), height=0.05, wrapWidth=None, ori=0, 
+    color='white', colorSpace='rgb', opacity=1, 
+    languageStyle='LTR',
+    depth=0.0);
+key_resp = keyboard.Keyboard()
 
 # Create some handy timers
 globalClock = core.Clock()  # to track the time since experiment started
@@ -177,6 +188,105 @@ for thisComponent in tela_inicialComponents:
 thisExp.addData('texto_tela_inicial.started', texto_tela_inicial.tStartRefresh)
 thisExp.addData('texto_tela_inicial.stopped', texto_tela_inicial.tStopRefresh)
 # the Routine "tela_inicial" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
+
+# ------Prepare to start Routine "instrucao_resposta"-------
+# update component parameters for each repeat
+key_resp.keys = []
+key_resp.rt = []
+# keep track of which components have finished
+instrucao_respostaComponents = [text_ir, key_resp]
+for thisComponent in instrucao_respostaComponents:
+    thisComponent.tStart = None
+    thisComponent.tStop = None
+    thisComponent.tStartRefresh = None
+    thisComponent.tStopRefresh = None
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+# reset timers
+t = 0
+_timeToFirstFrame = win.getFutureFlipTime(clock="now")
+instrucao_respostaClock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+frameN = -1
+continueRoutine = True
+
+# -------Run Routine "instrucao_resposta"-------
+while continueRoutine:
+    # get current time
+    t = instrucao_respostaClock.getTime()
+    tThisFlip = win.getFutureFlipTime(clock=instrucao_respostaClock)
+    tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
+    
+    # *text_ir* updates
+    if text_ir.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        text_ir.frameNStart = frameN  # exact frame index
+        text_ir.tStart = t  # local t and not account for scr refresh
+        text_ir.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(text_ir, 'tStartRefresh')  # time at next scr refresh
+        text_ir.setAutoDraw(True)
+    
+    # *key_resp* updates
+    waitOnFlip = False
+    if key_resp.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+        # keep track of start time/frame for later
+        key_resp.frameNStart = frameN  # exact frame index
+        key_resp.tStart = t  # local t and not account for scr refresh
+        key_resp.tStartRefresh = tThisFlipGlobal  # on global time
+        win.timeOnFlip(key_resp, 'tStartRefresh')  # time at next scr refresh
+        key_resp.status = STARTED
+        # keyboard checking is just starting
+        waitOnFlip = True
+        win.callOnFlip(key_resp.clock.reset)  # t=0 on next screen flip
+        win.callOnFlip(key_resp.clearEvents, eventType='keyboard')  # clear events on next screen flip
+    if key_resp.status == STARTED and not waitOnFlip:
+        theseKeys = key_resp.getKeys(keyList=['space'], waitRelease=False)
+        if len(theseKeys):
+            theseKeys = theseKeys[0]  # at least one key was pressed
+            
+            # check for quit:
+            if "escape" == theseKeys:
+                endExpNow = True
+            key_resp.keys = theseKeys.name  # just the last key pressed
+            key_resp.rt = theseKeys.rt
+            # a response ends the routine
+            continueRoutine = False
+    
+    # check for quit (typically the Esc key)
+    if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+        core.quit()
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
+    for thisComponent in instrucao_respostaComponents:
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
+    
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
+
+# -------Ending Routine "instrucao_resposta"-------
+for thisComponent in instrucao_respostaComponents:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+thisExp.addData('text_ir.started', text_ir.tStartRefresh)
+thisExp.addData('text_ir.stopped', text_ir.tStopRefresh)
+# check responses
+if key_resp.keys in ['', [], None]:  # No response was made
+    key_resp.keys = None
+thisExp.addData('key_resp.keys',key_resp.keys)
+if key_resp.keys != None:  # we had a response
+    thisExp.addData('key_resp.rt', key_resp.rt)
+thisExp.addData('key_resp.started', key_resp.tStartRefresh)
+thisExp.addData('key_resp.stopped', key_resp.tStopRefresh)
+thisExp.nextEntry()
+# the Routine "instrucao_resposta" was not non-slip safe, so reset the non-slip timer
 routineTimer.reset()
 
 # Flip one final time so any remaining win.callOnFlip() 
